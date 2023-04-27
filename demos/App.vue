823a5267704d143a199cd557adb9b4450c977e72
shenyuan.will <shenyuan.will@bytedance.com>
2023-04-27 19:00:39 +0800

    feat(virtual-cascader): 去除多余的样式

--- demos/App.vue
+++ demos/App.vue
@@ -23,9 +23,9 @@
 
 <script>
 import Mock from 'mockjs'
-import Cascader from './../dist/vue-virtual-cascader.umd.min.js';
-import './../dist/vue-virtual-cascader.css';
-// import Cascader from 'virtual-cascader/packages';
+// import Cascader from './../dist/vue-virtual-cascader.umd.min.js';
+// import './../dist/vue-virtual-cascader.css';
+import Cascader from 'virtual-cascader/packages';
 const { VirtualCascader } = Cascader;
 // import { getRender } from './const.jsx';
 
